280be19038337569bdd484786c6ca4a232c44246
xzl <1370914609@qq.com>
2020-11-30 16:46:02 +0800

    xzl

--- xcbase/xcedu-share.js
+++ xcbase/xcedu-share.js
@@ -53,7 +53,7 @@
 
     var toQueryString = function toQueryString(params) {
       return Object.keys(params).reduce(function (target, key) {
-        target.push("".concat(key, "=").concat(query[key]));
+        target.push("".concat(key, "=").concat(params[key]));
         return target;
       }, []).join('&');
     };
@@ -156,6 +156,55 @@
     clearToken: clearToken
   });
 
+  function _defineProperty(obj, key, value) {
+    if (key in obj) {
+      Object.defineProperty(obj, key, {
+        value: value,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      });
+    } else {
+      obj[key] = value;
+    }
+
+    return obj;
+  }
+
+  function ownKeys(object, enumerableOnly) {
+    var keys = Object.keys(object);
+
+    if (Object.getOwnPropertySymbols) {
+      var symbols = Object.getOwnPropertySymbols(object);
+      if (enumerableOnly) symbols = symbols.filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+      });
+      keys.push.apply(keys, symbols);
+    }
+
+    return keys;
+  }
+
+  function _objectSpread2(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i] != null ? arguments[i] : {};
+
+      if (i % 2) {
+        ownKeys(Object(source), true).forEach(function (key) {
+          _defineProperty(target, key, source[key]);
+        });
+      } else if (Object.getOwnPropertyDescriptors) {
+        Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+      } else {
+        ownKeys(Object(source)).forEach(function (key) {
+          Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+        });
+      }
+    }
+
+    return target;
+  }
+
   var instance = axios.create({
     baseURL: '/api/v1',
     headers: {
@@ -175,6 +224,16 @@
 
     if (token) {
       config.headers.Authorization = token;
+    }
+
+    if (config.method === 'post' || config.method === 'put') {
+      config.data = _objectSpread2(_objectSpread2({}, config.data), {}, {
+        _t: Date.parse(new Date()) / 1000
+      });
+    } else if (config.method === 'get' || config.method === 'delete') {
+      config.params = _objectSpread2(_objectSpread2({}, config.params), {}, {
+        _t: Date.parse(new Date()) / 1000
+      });
     }
 
     return config;
